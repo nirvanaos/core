36789e46a0e6f4f54fa8490ab0185165f11a47fd
Igor Popov <silver.popov@gmail.com>
2020-11-21 17:44:57 +0300

    Core g_memory object delegates to g_core_heap.

--- Objects/g_memory.cpp
+++ Objects/g_memory.cpp
@@ -1,12 +1,70 @@
 /// \file g_memory.cpp
 /// The g_memory object implementation.
 
-#include <user_memory.h>
+#include "core.h"
 #include <Nirvana/Memory_s.h>
 #include <Nirvana/OLF.h>
+#include "user_memory.h"
 
 namespace Nirvana {
 namespace Core {
+
+/// Delegates all operations to g_core_heap.
+class CoreMemory :
+	public ::CORBA::Nirvana::ServantStatic <CoreMemory, Memory>
+{
+public:
+	// Memory::
+	static void* allocate (void* dst, size_t size, UWord flags)
+	{
+		return g_core_heap.allocate (dst, size, flags);
+	}
+
+	static void release (void* p, size_t size)
+	{
+		return g_core_heap.release (p, size);
+	}
+
+	static void commit (void* p, size_t size)
+	{
+		return g_core_heap.commit (p, size);
+	}
+
+	static void decommit (void* p, size_t size)
+	{
+		return g_core_heap.decommit (p, size);
+	}
+
+	static void* copy (void* dst, void* src, size_t size, UWord flags)
+	{
+		return g_core_heap.copy (dst, src, size, flags);
+	}
+
+	static bool is_readable (const void* p, size_t size)
+	{
+		return g_core_heap.is_readable (p, size);
+	}
+
+	static bool is_writable (const void* p, size_t size)
+	{
+		return g_core_heap.is_writable (p, size);
+	}
+
+	static bool is_private (const void* p, size_t size)
+	{
+		return g_core_heap.is_private (p, size);
+	}
+
+	static bool is_copy (const void* p1, const void* p2, size_t size)
+	{
+		return g_core_heap.is_copy (p1, p2, size);
+	}
+
+	static intptr_t query (const void* p, MemQuery q)
+	{
+		return g_core_heap.query (p, q);
+	}
+};
 
 /// Delegates memory operations dependent on context.
 class UserMemory :
@@ -67,7 +125,7 @@ public:
 
 }
 
-extern const ImportInterfaceT <Memory> g_memory = { OLF_IMPORT_INTERFACE, nullptr, nullptr, STATIC_BRIDGE (Memory, Core::UserMemory) };
+extern const ImportInterfaceT <Memory> g_memory = { OLF_IMPORT_INTERFACE, nullptr, nullptr, STATIC_BRIDGE (Memory, Core::CoreMemory) };
 
 }
 
