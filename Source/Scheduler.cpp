f2385dd18280b038dabb0f9a055ecb7f0db45393
silver-popov <popov.nirvana@gmail.com>
2023-01-22 15:57:23 +0300

    Crash test

--- Source/Scheduler.cpp
+++ Source/Scheduler.cpp
@@ -55,12 +55,12 @@ void Scheduler::do_shutdown ()
 {
 	// Block incoming requests and complete currently executed ones.
 	PortableServer::Core::POA_Root::shutdown ();
-	int* p = nullptr;
-	*p = 0;
 	// Terminate services to release all proxies
 	CORBA::Core::Services::terminate ();
 	// Stop receiving messages
 	Port::PostOffice::terminate ();
+	int* p = nullptr;
+	*p = 0;
 	// If no activity - toggle it.
 	if (!global_->activity_cnt) {
 		global_->activity_cnt.increment ();
