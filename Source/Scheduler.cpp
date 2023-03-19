2e6d7afa4ba7c77485a36da4de7d7601124cf448
silver-popov <popov.nirvana@gmail.com>
2023-03-19 17:11:40 +0300

    DEBUG_SHUTDOWN

--- Source/Scheduler.cpp
+++ Source/Scheduler.cpp
@@ -30,11 +30,11 @@
 #include "Chrono.h"
 
 // Output debug messages on shutdown.
-//#define DEBUG_SHUTDOWN
+#define DEBUG_SHUTDOWN
 
 // If we use INFINITE_DEADLINE, the new background thread will be created for shutdown.
-#define SHUTDOWN_DEADLINE Chrono::make_deadline (1 * TimeBase::MINUTE)
-//#define SHUTDOWN_DEADLINE INFINITE_DEADLINE
+//#define SHUTDOWN_DEADLINE Chrono::make_deadline (1 * TimeBase::MINUTE)
+#define SHUTDOWN_DEADLINE INFINITE_DEADLINE
 
 namespace Nirvana {
 namespace Core {
