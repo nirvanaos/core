d673cdf04125ef8dc2fc7088bddcd024e97f36b3
Igor Popov <popov.nirvana@gmail.com>
2023-07-15 15:46:58 +0300

    Rename file

--- Source/FileAccessDirect.h
+++ Source/FileAccessDirect.h
@@ -29,7 +29,7 @@
 #pragma once
 
 #include <Nirvana/Nirvana.h>
-#include <Port/FileAccess.h>
+#include <Port/FileAccessDirect.h>
 #include "UserAllocator.h"
 #include "Chrono.h"
 #include "MapOrderedStable.h"
