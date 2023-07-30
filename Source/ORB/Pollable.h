0faa6b333a366aae02b30a0f6276b6583e9dcf61
Igor Popov <popov.nirvana@gmail.com>
2023-07-30 17:52:57 +0300

    + Pollable

---
+++ Source/ORB/Pollable.h
@@ -0,0 +1,52 @@
+/// \file
+/*
+* Nirvana Core.
+*
+* This is a part of the Nirvana project.
+*
+* Author: Igor Popov
+*
+* Copyright (c) 2021 Igor Popov.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Send comments and/or bug reports to:
+*  popov.nirvana@gmail.com
+*/
+#ifndef NIRVANA_ORB_CORE_POLLABLE_H_
+#define NIRVANA_ORB_CORE_POLLABLE_H_
+#pragma once
+
+#include <CORBA/CORBA.h>
+#include <CORBA/Pollable_s.h>
+
+namespace CORBA {
+namespace Core {
+
+class Pollable :
+	public Internal::ValueImpl <Pollable, CORBA::Pollable>,
+	public Internal::ValueImpl <Pollable, CORBA::ValueBase>,
+	public Internal::ValueTraits <Pollable>,
+	public Internal::ValueNonTruncatable,
+	public Internal::ValueBaseNoFactory
+{
+public:
+	bool is_ready (ULong timeout);
+	PollableSet::_ref_type create_pollable_set ();
+};
+
+}
+}
+
+#endif
