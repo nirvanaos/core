4bc7dc49173786cb19c71035e34290f5e3915f37
Igor Popov <popov.nirvana@gmail.com>
2023-08-22 13:49:55 +0300

    + AtomicCounter::increment_if_not_zero()

--- Source/AtomicCounter.h
+++ Source/AtomicCounter.h
@@ -139,6 +139,20 @@ public:
 		}
 	}
 
+	/// Increment counter if it is not zero.
+	/// 
+	/// \returns `true` if counter was incremented.
+	bool increment_if_not_zero () noexcept
+	{
+		IntegralType cur = cnt_.load (std::memory_order_relaxed);
+		for (;;) {
+			if (!cur)
+				return false;
+			if (cnt_.compare_exchange_weak (cur, cur + 1, std::memory_order_release, std::memory_order_relaxed))
+				return true;
+		}
+	}
+
 protected:
 	std::atomic <IntegralType> cnt_;
 };
