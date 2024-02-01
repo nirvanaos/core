dbb53c1c89a68043136d313be5677342192f0d85
Igor Popov <popov.nirvana@gmail.com>
2024-02-01 19:43:36 +0300

    + berkley-softfloat-3 module

--- SFloat/SFloat16.cpp
+++ SFloat/SFloat16.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include <CORBA/SFloat_s.h>
+#include "softfloat.h"
 
 namespace CORBA {
 namespace Internal {
@@ -7,24 +8,29 @@ namespace Internal {
 class Static_g_sfloat16 : public IDL::traits <SFloat16>::ServantStatic <Static_g_sfloat16>
 {
 public:
-	void from_double (const NativeDouble& x, FloatIEEE16& f);
-	void from_long_double (const NativeLongDouble& x, FloatIEEE16& f);
-	void from_long (long x, FloatIEEE16& f);
-	void from_unsigned_long (unsigned long x, FloatIEEE16& f);
-	void from_long_long (long long x, FloatIEEE16& f);
-	void from_unsigned_long_long (unsigned long long x, FloatIEEE16& f);
+	static void from_double (const NativeDouble& x, FloatIEEE16& f);
 
-	void to_double (const FloatIEEE16& f, NativeDouble& x);
-	void to_long_double (const FloatIEEE16& f, NativeLongDouble& x);
-	void to_long (const FloatIEEE16& f, long& x);
-	void to_long_long (const FloatIEEE16& f, long long& x);
+	static void from_long_double (const NativeLongDouble& x, FloatIEEE16& f)
+	{
+		extF80M_to_f128M (&reinterpret_cast <const extFloat80_t&> (x), &reinterpret_cast <float128_t&> (f));
+	}
 
-	void sum (FloatIEEE16& x, const FloatIEEE16& y);
-	void sub (FloatIEEE16& x, const FloatIEEE16& y);
-	void mul (FloatIEEE16& x, const FloatIEEE16& y);
-	void div (FloatIEEE16& x, const FloatIEEE16& y);
+	static void from_long (long x, FloatIEEE16& f);
+	static void from_unsigned_long (unsigned long x, FloatIEEE16& f);
+	static void from_long_long (long long x, FloatIEEE16& f);
+	static void from_unsigned_long_long (unsigned long long x, FloatIEEE16& f);
 
-	int compare (const FloatIEEE16& x, const FloatIEEE16& y);
+	static void to_double (const FloatIEEE16& f, NativeDouble& x);
+	static void to_long_double (const FloatIEEE16& f, NativeLongDouble& x);
+	static int32_t to_long (const FloatIEEE16& f);
+	static int64_t to_long_long (const FloatIEEE16& f);
+
+	static void sum (FloatIEEE16& x, const FloatIEEE16& y);
+	static void sub (FloatIEEE16& x, const FloatIEEE16& y);
+	static void mul (FloatIEEE16& x, const FloatIEEE16& y);
+	static void div (FloatIEEE16& x, const FloatIEEE16& y);
+
+	static int compare (const FloatIEEE16& x, const FloatIEEE16& y);
 };
 
 }
