79f07443a03e44d6c81d08ef66f167b6da6b41ac
Igor Popov <popov.nirvana@gmail.com>
2024-05-30 14:07:58 +0300

    Start register_module implementation

--- Source/Packages.h
+++ Source/Packages.h
@@ -102,14 +102,12 @@ public:
 
 	void register_module (const CosNaming::Name& path, unsigned flags)
 	{
-		/*
 		CORBA::Object::_ref_type obj;
 		name_service_->bind (path, obj);
 		Nirvana::File::_ref_type file = Nirvana::File::_narrow (obj);
 		AccessBuf::_ref_type acc = AccessBuf::_downcast (file->open (O_RDONLY, 0)->_to_value ());
 		Stream stm (acc);
 		ModuleMetadata metadata = get_module_metadata (stm);
-		*/
 	}
 
 	static IDL::traits <AccessDirect>::ref_type open_binary (CosNaming::NamingContextExt::_ptr_type ns,
